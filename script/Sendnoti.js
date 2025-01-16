fd73ea12399ba081d056bd6e14d9e60b08ab484e
Yhana alvarez <yasisarmia@gmail.com>
2025-01-17 01:03:08 +0800

    Update Sendnoti.js

--- script/Sendnoti.js
+++ script/Sendnoti.js
@@ -23,7 +23,7 @@ module.exports.run = async function ({ api, event, args }) {
 			await api.sendMessage(
 				`𝙉𝙊𝙏𝙄𝘾𝙀 𝙁𝙍𝙊𝙈 𝘿𝙀𝙑𝙀𝙇𝙊𝙋𝙀𝙍 
  ---------------- 
- Developer Name : Alex Jhon M. Ponce
+ Developer Name : Bioscope-08
  --------------- 
  『𝗡𝗼𝘁𝗶𝗰𝗲』${custom}`,
 				thread.threadID
