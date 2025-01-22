d58b35cece6c0d2213e3ac740ee617641ab281e1
Yhana alvarez <yasisarmia@gmail.com>
2025-01-23 00:31:36 +0800

    Update help.js

--- script/help.js
+++ script/help.js
@@ -25,7 +25,7 @@ module.exports.run = async function({
       let page = 1;
       let start = (page - 1) * pages;
       let end = start + pages;
-      let helpMessage = `📘available Command List:\n\n`;
+      let helpMessage = `available Command List:\n\n`;
       for (let i = start; i < Math.min(end, commands.length); i++) {
         helpMessage += `\t${i + 1}. 「 ${prefix}${commands[i]} 」\n`;
       }
